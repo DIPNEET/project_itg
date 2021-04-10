f3cd32a4f2135585ffa84d313ab681d476b8810c
dipneet <dksandhu97@gmail.com>
2021-04-10 19:28:58 +0530

    project_itg

---
+++ src/components/dummy.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Dummy extends Component {
+  render() {
+    return(
+        <div>
+            <h1>LOReM IPSUM</h1>
+        </div>
+    )
+
+}
+}
---
+++ src/components/home.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+  render() {
+    return(
+        <div>
+            <div class="Nav">
+            <div class="grp15">
+                <img src="Icons/Group 15.svg"/>
+                <p></p>
+            </div>
+            <div class="menu">
+                <p class="text">ITG LIVE</p>
+                <p class="divider"></p>
+                <div class="Nav2">
+                    <div class="frame831_1">
+                    <a href="#" target="_blank" class="frameInside2">Home</a>
+                    </div>
+                
+                    <div class="frame831_2">
+                    <a href="#" target="_blank" class="frameInside2">Services</a>
+                </div>
+               
+                    <div class="frame831_3">
+                    <a href="#" target="_blank" class="frameInside3">Pricing</a>
+                   
+                </div>
+                
+                    <div class="frame831_4">
+                    <a href="#" target="_blank" class="frameInside4">Support</a>
+                    </div>
+                   
+                    
+                       
+                        
+                       
+                </div>
+            </div>
+            <div class="icons">
+                <div class="socialIcons">
+                    <div class="socialf">
+                    <a href="#" target="_blank"><img src="Icons/Facebook/Line.png"/></a>
+                </div>
+                    <div class="sociali">
+                    <a href="#" target="_blank"><img src="Icons/Instagram/Line.png"/></a>
+                </div>
+                    <div class="socialt">
+                    <a href="#" target="_blank"><img src="Icons/Twitter/Line.png"/></a>
+                    </div>
+                </div>
+                <button id="button" class="b" ><span class="label">Members Login</span></button>
+                <script type="text/javascript">
+                   {/* { document.getElementById("button").onclick = function () {
+                        
+                   }
+                  } */}
+                    
+                </script>
+            </div>
+        </div>
+
+         
+         <div class="group">
+            <div class="group46061">
+                <img src="Icons/Group46061.png"/>
+            </div>
+            <div class="group46">
+                <img src="Icons/Group.png"/>
+            </div> 
+        </div>
+
+        </div>
+
+    )
+
+  }
+}
