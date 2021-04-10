f95340da7f5e4fb73ba5f7be9cbf4bb13df84711
dipneet <dksandhu97@gmail.com>
2021-04-10 20:24:46 +0530

    project_itg

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -15,20 +15,20 @@ export default class Home extends Component {
                 <p class="divider"></p>
                 <div class="Nav2">
                     <div class="frame831_1">
-                    <a href="#" target="_blank" class="frameInside2">Home</a>
+                    <a href="" target="_blank" class="frameInside2">Home</a>
                     </div>
                 
                     <div class="frame831_2">
-                    <a href="#" target="_blank" class="frameInside2">Services</a>
+                    <a href="" target="_blank" class="frameInside2">Services</a>
                 </div>
                
                     <div class="frame831_3">
-                    <a href="#" target="_blank" class="frameInside3">Pricing</a>
+                    <a href="" target="_blank" class="frameInside3">Pricing</a>
                    
                 </div>
                 
                     <div class="frame831_4">
-                    <a href="#" target="_blank" class="frameInside4">Support</a>
+                    <a href="" target="_blank" class="frameInside4">Support</a>
                     </div>
                    
                     
@@ -40,13 +40,13 @@ export default class Home extends Component {
             <div class="icons">
                 <div class="socialIcons">
                     <div class="socialf">
-                    <a href="#" target="_blank"><img src="Icons/Facebook/Line.png"/></a>
+                    <a href="" target="_blank"><img src="Icons/Facebook/Line.png"/></a>
                 </div>
                     <div class="sociali">
-                    <a href="#" target="_blank"><img src="Icons/Instagram/Line.png"/></a>
+                    <a href="" target="_blank"><img src="Icons/Instagram/Line.png"/></a>
                 </div>
                     <div class="socialt">
-                    <a href="#" target="_blank"><img src="Icons/Twitter/Line.png"/></a>
+                    <a href="" target="_blank"><img src="Icons/Twitter/Line.png"/></a>
                     </div>
                 </div>
                 <button id="button" class="b" ><span class="label">Members Login</span></button>
